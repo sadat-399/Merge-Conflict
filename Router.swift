102e10fac9017ddccfaab9c39dd458170dd9678d
Sadat Ahmed <sadat.ahmed@bjitgroup.com>
2025-03-24 10:03:15 +0600

    dev2

--- Router.swift
+++ Router.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+// This class is responsible for routing to the next view controller
+// It will have the logic to navigate to the next view controller
+// It will have the logic to pass data to the next view controller
+// It will have the logic to create the next view controller
+// It will have the logic to present the next view controller
 class Router: RouterInput {
     func showArticleDetail(from view: ViewInput, for article: Article) {
         if let view = view as? ArticleListView {
