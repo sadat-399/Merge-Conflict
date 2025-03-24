4ebbf04b8b215039eaa6f107aac91159abd030c0
Sadat Ahmed <sadat.ahmed@bjitgroup.com>
2025-03-24 10:10:36 +0600

    development

--- Router.swift
+++ Router.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 class Router: RouterInput {
+    // MARK: PROPERTIES
+    weak var view: ViewInput?
+    // MARK: FUNCTIONS
     func showArticleDetail(from view: ViewInput, for article: Article) {
+        // Create article detail module and present it
+    }
+    // MARK: FUNCTIONS
+    func showArticleDetail(from view: ViewInput, for article: Article) {
+        // Create article detail module and present it
+
         if let view = view as? ArticleListView {
             view.showArticleDetail(article: article)
         }
